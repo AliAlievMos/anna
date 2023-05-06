319bb07d6f4e34a591e7717ae0ee003e67548d24
Али <ali@MacBook-Pro-Ali.local>
2023-05-06 22:07:29 +0300

    initial dev

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const replaceBtn = document.getElementById('replaceBtn');
-const testButton = document.getElementById('testButton');
 const backBtn = document.getElementById('backBtn');
-const originalHTML = document
+// const originalHTML = document
 const dropSvg = ['<img src="drop1.png" alt="Description of your image" width="15" height="auto">',
                             '<img src="drop2.png" alt="Description of your image" width="15" height="auto">',
                             '<img src="drop3.png" alt="Description of your image" width="35" height="auto">']
@@ -30,7 +29,6 @@ backBtn.addEventListener('click', ()=>{
     location.reload();
     }
 );
-
 // function revertHTML() {
 //     document.body.innerHTML = originalHTML;
 // }
@@ -42,3 +40,79 @@ function randomInt(){
 function randomDrop(){
     return Math.floor(Math.random() * 3)
 }
+//
+const color = 'black';
+
+const canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mousedown', setPosition);
+    canvas.addEventListener('mouseenter', setPosition);
+
+    let x, y;
+
+    function setPosition(event) {
+        x = event.clientX - canvas.offsetLeft;
+        y = event.clientY - canvas.offsetTop;
+    }
+
+    function draw(event) {
+        if (event.buttons !== 1) return;
+
+        context.beginPath();
+        context.strokeStyle = color;
+        context.lineWidth = 2;
+        context.lineCap = 'round';
+        context.moveTo(x, y);
+        setPosition(event);
+        context.lineTo(x, y);
+        context.stroke();
+    }
+
+function getCanvasPosition(canvas) {
+    const rect = canvas.getBoundingClientRect();
+    const doc = document.documentElement;
+    const left = rect.left + (window.pageXOffset || doc.scrollLeft);
+    const top = rect.top + (window.pageYOffset || doc.scrollTop);
+    return { left, top };
+}
+
+function updateCanvasPosition() {
+    const { left, top } = getCanvasPosition(canvas);
+    canvas.style.left = left + 'px';
+    canvas.style.top = top + 'px';
+}
+
+window.addEventListener('scroll', updateCanvasPosition);
+
+
+// 1
+
+// const canvas1 = document.getElementById('canvas1');
+// const context1 = canvas1.getContext('2d');
+//     canvas1.addEventListener('mousemove', draw1);
+//     canvas1.addEventListener('mousedown', setPosition1);
+//     canvas1.addEventListener('mouseenter', setPosition1);
+//
+//     let x1, y1;
+//
+//     function setPosition1(event, canavas) {
+//         x1 = event.clientX - canvas1.offsetLeft;
+//         y1 = event.clientY - canvas1.offsetTop;
+//     }
+//
+//     function draw1(event) {
+//         if (event.buttons !== 1) return;
+//
+//         context1.beginPath();
+//         context1.strokeStyle = color;
+//         context1.lineWidth = 2;
+//         context1.lineCap = 'round';
+//         context1.moveTo(x, y);
+//         setPosition1(event);
+//         context1.lineTo(x, y);
+//         context1.stroke();
+//     }
+// }
+
