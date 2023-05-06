f956c24c030b2714713c4234b0cd11b56a7055e7
Али <ali@MacBook-Pro-Ali.local>
2023-05-06 19:18:47 +0300

    initial

--- script.js
+++ script.js
@@ -1,113 +1,45 @@
-// const text = document.querySelector('#text');
-// const svg = "./get"
-//
-// // define an array of SVG paths to use
-// const svgs = [
-//     './Капля2.png',
-//     'M20,0 L0,20 L20,20 Z',
-//     'M0,0 L20,0 L10,20 Z',
-//     'M0,20 L20,20 L10,0 Z'
-// ];
-//
-// // loop through each letter in the text string
-// for (let i = 0; i < text.textContent.length; i++) {
-//     const letter = text.textContent[i];
-//     // replace the letter with an SVG image
-//         if (letter !== ' ') {
-//             if (i === 0) {
-//                     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-//                     svg.setAttribute('viewBox', '0 0 20 20');
-//
-//                     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-//                     path.setAttribute('d', svgs[Math.floor(Math.random() * svgs.length)]);
-//                     path.setAttribute('fill', 'red');
-//
-//                     svg.appendChild(path);
-//
-//                     const span = document.createElement('span');
-//                     span.appendChild(svg);
-//
-//                     // replace the letter with the new span element
-//                     text.innerHTML = text.innerHTML.replace(letter, span.outerHTML);
-//         }
-//     }
-// }
-//
-// function randomInt(){
-//     return Math.floor(Math.random() * 1146)
-// }
-
-// const text = document.querySelector('#text');
-// const letters = text.textContent.split('');
-//
-// const myParagraph = document.getElementById('button'); // get the <p> element
-//
-// myParagraph.addEventListener('click', function() { // add a 'click' event listener
-//
-// // create an XMLHttpRequest object
-// const xhr = new XMLHttpRequest();
-// // specify the URL of the SVG image file
-// xhr.open('GET', './drop.svg');
-// // set the responseType to document so that the response is parsed as an XML document
-// xhr.responseType = 'document';
-// // register a callback for when the request completes
-//     // get the SVG element from the response
-//     const svg = xhr.responseXML.documentElement;
-//
-//     // loop through each letter in the text
-//     for (let i = 0; i < letters.length; i++) {
-//         const letter = letters[i];
-//
-//         if (letter !== ' ' && i === 0)   {
-//             // clone the SVG element
-//             svg.setAttribute('width', '15px')
-//             svg.setAttribute('height', '15px')
-//             const clone = svg.cloneNode(true);
-//             // set the fill color of the SVG element to red
-//             clone.querySelector('path');
-//             const span = document.createElement('span');
-//             span.appendChild(clone);
-//
-//             // replace the letter with the new span element
-//             text.innerHTML = text.innerHTML.replace(letter, span.outerHTML);
-//         }
-//     }
-//
-// xhr.send()
-// });
-//
-//
-//
-// function randomInt(){
-//     return Math.floor(Math.random() * 1146)
-// }
-
-// Array of letters to be replaced with SVG images
-const lettersToReplace = ['o'];
-
-// Function to replace letters with SVG images
-function replaceLetters() {
-    // Select all HTML elements that contain the text to be replaced
-    const elements = document.querySelectorAll('#text');
-
-    // Loop through each selected element
-    elements.forEach(element => {
-        // Get the text content of the element
-        let content = element.innerHTML;
-
-        // Replace selected letters with SVG image tags
-        lettersToReplace.forEach(letter => {
-            content = content.replace(new RegExp(letter, 'gi'), '<img src="./drop.svg" width="25px" height="auto" alt="SVG image" />');
+const replaceBtn = document.getElementById('replaceBtn');
+const testButton = document.getElementById('testButton');
+const backBtn = document.getElementById('backBtn');
+const originalHTML = document
+const dropSvg = ['<img src="drop1.png" alt="Description of your image" width="15" height="auto">',
+                            '<img src="drop2.png" alt="Description of your image" width="15" height="auto">',
+                            '<img src="drop3.png" alt="Description of your image" width="35" height="auto">']
+replaceBtn.addEventListener('click', () => {
+    console.log("repl")
+    let text = document.getElementById('text');
+    let divs =    text.querySelectorAll("div");
+    divs.forEach(div => {
+        let ps = div.querySelectorAll("p")
+        ps.forEach(p => {
+            let letters = p.textContent.split('');
+            for (let i = 0; i < letters.length; i++) {
+                let letter = letters[i];
+                if (letter !== ' ') {
+                    if (randomInt() % 56 === 0) {
+                        letters[i] = dropSvg[randomDrop()]
+                    }
+                }
+            }
+            p.innerHTML = letters.join('')
         });
-
-        // Update the innerHTML property of the element with the new content
-        element.innerHTML = content;
     });
-}
+});
 
-// Add event listener to the button
-document.getElementById('replace-btn').addEventListener('click', replaceLetters);
+backBtn.addEventListener('click', ()=>{
+    location.reload();
+    }
+);
+testButton.addEventListener('click',() => {console.log("12")});
+
+// function revertHTML() {
+//     document.body.innerHTML = originalHTML;
+// }
+
 
 function randomInt(){
-    return Math.floor(Math.random() * 5)
+    return Math.floor(Math.random() * 1146)
+}
+function randomDrop(){
+    return Math.floor(Math.random() * 3)
 }
